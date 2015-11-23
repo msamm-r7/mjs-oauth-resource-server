40158d8c50b53e9eb4271cd056ab58999b52b17e
msamm-r7 <martin_samm@rapid7.com>
2015-11-23 21:10:49 +0000

    more split testing

--- src/main/java/com/rapid7/poc/oauth/config/Oauth2ResourceServerConfiguration.java
+++ src/main/java/com/rapid7/poc/oauth/config/Oauth2ResourceServerConfiguration.java
@@ -1,10 +1,13 @@
 package com.rapid7.poc.oauth.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 
 public class Oauth2ResourceServerConfiguration {
 
@@ -14,11 +17,26 @@ public class Oauth2ResourceServerConfiguration {
     @EnableResourceServer
     protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
+        /**
+         * Use the remote token services in the Authorisation Server.
+         * @return
+         */
+        @Bean
+        public ResourceServerTokenServices tokenService() {
+           RemoteTokenServices tokenServices = new RemoteTokenServices();
+           tokenServices.setClientId("clientapp");
+           tokenServices.setClientSecret("123456");
+           tokenServices.setCheckTokenEndpointUrl("http://localhost:9099/oauth/check_token");
+           return tokenServices;
+        }        
+        
         @Override
         public void configure(ResourceServerSecurityConfigurer resources) {
             // @formatter:off
             resources
-                .resourceId(RESOURCE_SERVER_RESOURCE_ID);
+                .resourceId(RESOURCE_SERVER_RESOURCE_ID)
+                .tokenServices(tokenService())
+                ;
             // @formatter:on
         }
 
